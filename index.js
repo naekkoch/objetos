543dfe60ae72a19d5908bb408956f35fe15c7b25
naekkoch <micaelaas3105@gmail.com>
2023-08-30 21:41:52 -0300

    exercicio29642797

---
+++ index.js
@@ -0,0 +1,50 @@
+// exercicio 1: Será impresso "Matheus Nachtergaele" dizendo seu comprimento e dizendo que será transmitido na globo as 14h
+//exercicio 2: cachorro - juca, 3, srd
+// gato - juba
+// tartaruga - jubo
+//ele serve para copiar e modificar
+//exercicio 3: False e indefinido
+//pois não colocaram uma propriedade chamada altura
+/*
+const Nae = {
+    nome: "Micaela", 
+    apelidos: ["Mica", "Nae", "Naenae"]
+ }
+
+const frase = (objeto) => {
+ console.log("Eu sou a", objeto.nome, "mas pode me chamar de", objeto.apelidos)
+}
+
+frase(Nae)
+
+exercicio 2:
+
+const pessoaUm = {
+    nome: "Erick",
+    idade: 45,
+    profissão: "morador de rua"
+}
+
+const pessoaDois = {
+    nome: "Rafael",
+    idade: 70,
+    profissão: "Fullstack e professor"
+}
+
+arrayMaker = (objeto) => {
+    array = []
+    array.push(objeto.nome)
+    array.push(objeto.nome.length)
+    array.push(objeto.idade)
+    array.push(objeto.profissão)
+    array.push(objeto.profissão.length)
+    console.log(array)
+}
+
+arrayMaker(pessoaUm)
+arrayMaker(pessoaDois)
+
+Não consigo fazer o exercicio 3 plmds
+*/
+
+
